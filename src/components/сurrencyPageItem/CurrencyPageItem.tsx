051e1ad6b6fb7b6b94cc91d08aedd8efd0ce09b6
unknown <poplawskyiihor@gmail.com>
2023-04-27 10:56:17 +0300

    added laws

--- src/components/сurrencyPageItem/CurrencyPageItem.tsx
+++ src/components/сurrencyPageItem/CurrencyPageItem.tsx
@@ -4,14 +4,18 @@ import { Icurr } from '../../types'
 import s from './CurrencyPageItem.module.scss'
 
 export const CurrencyPageItem: FC<Icurr> = ({ rate, exchangedate, txt }): JSX.Element => {
-
+  
   return (
     <div className={s.container}>
-      <div className={s.item}>
-        <span>{txt}</span>
-        <span>{rate}</span>
-        <span>{exchangedate}</span>
-      </div>
+        <div className={s.item}>
+          <div>{txt}</div>
+        </div>
+        <div className={s.item}>
+          <div>{rate} грн</div>
+        </div>
+        <div className={s.item}>
+          <div>{exchangedate}</div>
+        </div>
     </div>
   )
 }
